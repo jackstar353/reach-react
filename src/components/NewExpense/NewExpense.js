e574487b700d346d895f9882f1de4d1d3b2ae091
jackstar353 <michellescully2014@gmail.com>
2021-12-04 23:12:39 +0000

    Added cancel button and close form functionality

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,21 +1,38 @@
-import './NewExpense.css'
-import ExpenseForm from './ExpenseForm';
-import ExpenseDate from '../Expenses/ExpenseDate';
+import React, { useState } from "react";
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
 
-const NewExpense = (props) =>{
-    const saveExpenseDataHandler =(enteredExpenseData)=> {
-        const expenseData = {
-            ...enteredExpenseData,
-            id:Math.random().toString()
-        };
-        console.log(expenseData)
-        props.onAddExpense(expenseData)
+const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
     };
-    return (
-        <div className="new-expense"> 
-        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+    props.onAddExpense(expenseData);
+    setIsEditing(false)
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+  return (
+    <div className="new-expense">
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
-    );
+  );
 };
 
 export default NewExpense;
