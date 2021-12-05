61cf7ef5061edcb1157f8a7fcc5cd512fd659df6
jackstar353 <michellescully2014@gmail.com>
2021-12-05 00:59:05 +0000

    Added fill to chart based on expenses amount

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -9,8 +9,9 @@ const ChartBar = (props) => {
   return (
     <div className="chart_bar">
       <div className="chart-bar__inner">
-        <div className="chart-bar__fill" style={{ height: barFillHeight }}>
-          {" "}
+        <div className="chart-bar__fill" 
+        style={{ height: barFillHeight }}>
+          
         </div>
       </div>
       <div className="chart-bar__label">{props.label}</div>
